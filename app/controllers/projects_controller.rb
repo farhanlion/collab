e30569df19d075c56cf1b292fa5359847fbba0f3
Farhan Ahsan <bbss1e3farhan@gmail.com>
2020-08-06 21:45:14 +0800

    project model view controller created

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,22 @@
 class ProjectsController < ApplicationController
+    def index
+        @projects = Project.all
+    end
+
+    def show
+        @project = Project.find(params[:id])
+    end
+
     def new
     end
    
     def create
+        @project = Project.new(article_params)
+        @project.save
+        redirect_to @project
     end
+    private
+        def article_params
+            params.require(:project).permit(:title, :description)
+        end
 end
